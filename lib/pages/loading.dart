e5e6e65186efbd632fa17c0ba24353ae8963f327
nipunarat <nipunarat1997@gmail.com>
2021-03-20 01:12:52 +0530

    created separate class for world_time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async{
+    //make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Colombo');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offsetHour = data['utc_offset'].substring(1,3);
+    String offsetMinute = data['utc_offset'].substring(4,6);
+    // print(offsetHour);
+    // print(offsetMinute);
+    // print(datetime);
+
+    //create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offsetHour),minutes: int.parse(offsetMinute)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
